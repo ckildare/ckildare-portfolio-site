cc6fc2c263a6cdbcc490c5d664eab379ab898feb
Connor Kildare <98177297+ckildare@users.noreply.github.com>
2026-02-10 20:03:36 -0600

    a

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,6 @@
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import Head from 'next/head';
-import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return <>
