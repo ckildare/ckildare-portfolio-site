4b38bb5ad4b4051adff138f87210d948058f1d16
Connor Kildare <98177297+ckildare@users.noreply.github.com>
2026-02-10 19:37:12 -0600

    Add speed insights

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Head from 'next/head';
 import '../styles/globals.css';
 
@@ -17,5 +18,6 @@ export default function App({ Component, pageProps }) {
     </Head>
     <Component {...pageProps} />
     <Analytics />
+    <SpeedInsights />
   </>
 }
